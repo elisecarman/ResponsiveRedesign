4faa11a0f92caa9cdd53cb6433b1ebd85c76cc07
Elise Carman <elise.carman1@gmail.com>
2025-02-23 17:45:54 -0500

    desperate

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/elisecarman.github.io/interactiondesign",
+  base: "/elisecarman.github.io/InteractionDesign-New/",
   plugins: [react()],
 })
